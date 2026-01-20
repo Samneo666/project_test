b0130ce5bbbf6500e5db996c0ae62ac0df373794
Sam <britpop1992@gmail.com>
2026-01-20 14:37:49 +0800

    [inttialize] 專題 初始化!

--- src/main/java/web/member/controller/LoginController.java
+++ src/main/java/web/member/controller/LoginController.java
@@ -1,5 +1,21 @@
 package web.member.controller;
 
-public class LoginController {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/member/login")
+public class LoginController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("專題測試");
+	}
+	
 
 }
